3198f69a382df35c6450c1e42dab4299776bf6c2
dawnap <dawnap24@gmail.com>
2017-02-09 22:30:01 +0100

    0.0.2

--- src/main/webapp/resources/js/map.js
+++ src/main/webapp/resources/js/map.js
@@ -1,93 +1,102 @@
-/*jslint browser: true*/
-/*global google,console,showSportField,angular*/
-
 function initAutocomplete() {
-	"use strict";
 
-	var map = new google.maps.Map(document.getElementById('map'), {center: {lat: 51.8335857, lng: 14.6499367}, zoom: 5, mapTypeId: 'roadmap'}),
-		input = document.getElementById('pac-input'),
-		searchBox = new google.maps.places.SearchBox(input),
-		sportFields = [],
-		markers = [],
-		i;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 51.8335857, lng: 14.6499367},
+    zoom: 5,
+    mapTypeId: 'roadmap'
+  });
 
-	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-	map.addListener('bounds_changed', function () {
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-		searchBox.setBounds(map.getBounds());
-	});
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
 
-
-	sportFields = JSON.parse(localStorage.getItem("sportFields"));
-	console.log(sportFields);
-
-	for (i = 0; i < sportFields.length; i += 1) {
-		markers.push(
-			new google.maps.Marker({
-				position: new google.maps.LatLng(sportFields[i].lat, sportFields[i].lng),
-				map: map,
-				title: 'Hello World!',
-				id: sportFields[i].id
-			})
-		);
-	}
-
-	markers.forEach(function (marker) {
-		google.maps.event.addListener(marker, 'click', function () {
-			map.setZoom(14);
-			map.setCenter(marker.getPosition());
-			showSportField(marker.id);
-		});
-	});
-
-	searchBox.addListener('places_changed', function () {
+    searchBox.setBounds(map.getBounds());
+  });
 
 
-		var places = searchBox.getPlaces(),
-			bounds = new google.maps.LatLngBounds();
+    var sportFields = [];
+    sportFields = JSON.parse(localStorage.getItem("sportFields"));
+    console.log(sportFields);
+  var markers = [];
 
-		if (places.length === 0) {
-			return;
-		}
+    for (i = 0; i < sportFields.length; i++) {
+        markers.push(
+        new google.maps.Marker({
+          position: new google.maps.LatLng(sportFields[i].lat,sportFields[i].lng),
+          map: map,
+          title: 'Hello World!',
+          id: sportFields[i].id
+        }));
+    }
 
-		markers.forEach(function (marker) {
-			marker.setMap(marker);
-		});
-		markers = [];
+     markers.forEach(function(marker) {
+      google.maps.event.addListener(marker, 'click', function() {
+           map.setZoom(14);
+          map.setCenter(marker.getPosition());
+          showSportField(marker.id);
+        });
+    });
 
-		places.forEach(function (place) {
-			if (!place.geometry) {
-				console.log("Returned place contains no geometry");
-				return;
-			}
-			var icon = {
-				url: place.icon,
-				size: new google.maps.Size(71, 71),
-				origin: new google.maps.Point(0, 0),
-				anchor: new google.maps.Point(17, 34),
-				scaledSize: new google.maps.Size(25, 25)
-			};
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
 
-			markers.push(new google.maps.Marker({
-				map: map,
-				icon: icon,
-				title: place.name,
-				position: place.geometry.location
-			}));
 
-			if (place.geometry.viewport) {
-				bounds.union(place.geometry.viewport);
-			} else {
-				bounds.extend(place.geometry.location);
-			}
-		});
-		map.fitBounds(bounds);
-		map.setZoom(14);
-	});
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(marker);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+    map.setZoom(14);
+  });
+
+
+
 }
 
-function showSportField(sportField) {
-	"use strict";
-	var scope = angular.element(document.getElementById("mainMap")).scope();
-	scope.show(sportField);
+function showSportField(sportField){
+     var scope = angular.element(document.getElementById("mainMap")).scope();
+     scope.show(sportField);
 }
--- src/main/webapp/resources/js/sportfield.js
+++ src/main/webapp/resources/js/sportfield.js
@@ -1,98 +1,102 @@
-/*jslint browser: true*/
-/*global google,console*/
-
 function initAutocomplete() {
-    "use strict";
-    var map = new google.maps.Map(document.getElementById('map'), {center: {lat: 51.8335857, lng: 14.6499367}, zoom: 5, mapTypeId: 'hybrid'}),
-        newSportField,
-        input = document.getElementById('pac-input'),
-        searchBox = new google.maps.places.SearchBox(input),
-        sportFields = [],
-        markers = [],
-        i;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 51.8335857, lng: 14.6499367},
+    zoom: 5,
+    mapTypeId: 'hybrid'
 
-    map.setOptions({draggableCursor: 'crosshair'});
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-    map.addListener('bounds_changed', function () {
-        searchBox.setBounds(map.getBounds());
-    });
+  });
+    var newSportField;
+    map.setOptions({draggableCursor:'crosshair'});
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+
+    searchBox.setBounds(map.getBounds());
+  });
+
+    var sportFields = [];
     sportFields = JSON.parse(localStorage.getItem("sportFields"));
     console.log(sportFields);
-
-    for (i = 0; i < sportFields.length; i += 1) {
-        google.maps.Marker({
-            position: new google.maps.LatLng(sportFields[i].lat, sportFields[i].lng),
-            map: map,
-            title: 'Hello World!'
+  var markers = [];
+    for (i = 0; i < sportFields.length; i++) {
+        new google.maps.Marker({
+          position: new google.maps.LatLng(sportFields[i].lat,sportFields[i].lng),
+          map: map,
+          title: 'Hello World!'
         });
     }
 
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-    searchBox.addListener('places_changed', function () {
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
 
 
-        var places = searchBox.getPlaces(),
-            bounds = new google.maps.LatLngBounds();
+    var places = searchBox.getPlaces();
 
-        if (places.length === 0) {
-            return;
-        }
+    if (places.length == 0) {
+      return;
+    }
 
-        markers.forEach(function (marker) {
-            marker.setMap(marker);
-        });
-        markers = [];
-
-        places.forEach(function (place) {
-            if (!place.geometry) {
-                console.log("Returned place contains no geometry");
-                return;
-            }
-            var icon = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25)
-            };
-
-            // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-                map: map,
-                icon: icon,
-                title: place.name,
-                position: place.geometry.location
-            }));
-
-            if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-            } else {
-                bounds.extend(place.geometry.location);
-            }
-        });
-        map.fitBounds(bounds);
-        map.setZoom(14);
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(marker);
     });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+    map.setZoom(14);
+  });
 
 
-    google.maps.event.addListener(map, "click", function (event) {
-        if (newSportField !== null) {
-            newSportField.setMap(null);
-        }
-        var lat = event.latLng.lat(),
-            lng = event.latLng.lng();
-        // populate yor box/field with lat, lng
+  google.maps.event.addListener(map, "click", function(event) {
+    if(newSportField != null){
+        newSportField.setMap(null);
+    }
+    var lat = event.latLng.lat();
+    var lng = event.latLng.lng();
+    // populate yor box/field with lat, lng
 
-        document.getElementById('lat').value = lat;
-        document.getElementById('lng').value = lng;
-        newSportField = new google.maps.Marker({
-            position: new google.maps.LatLng(lat, lng),
-            map: map,
-            title: 'Hello World!'
+      document.getElementById('lat').value = lat;
+       document.getElementById('lng').value = lng;
+    newSportField = new google.maps.Marker({
+          position: new google.maps.LatLng(lat, lng),
+          map: map,
+          title: 'Hello World!'
         });
 
-    });
+  });
 }
