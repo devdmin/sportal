b19756b8befd5d115bd6f221ec5f229e86dffc72
devdmin <devdmin@protonmail.ch>
2017-03-11 14:10:30 +0100

    deafult admin account added

--- src/main/java/com/devdmin/PlayNowApplication.java
+++ src/main/java/com/devdmin/PlayNowApplication.java
@@ -1,16 +1,29 @@
 package com.devdmin;
 
 import com.devdmin.core.model.User;
+import com.devdmin.core.model.util.Gender;
+import com.devdmin.core.service.UserService;
 import com.devdmin.core.service.impl.UserServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class PlayNowApplication {
 
 	
 	public static void main(String[] args) {
-		SpringApplication.run(PlayNowApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(PlayNowApplication.class, args);
+		User user = new User();
+		user.setUsername("admin");
+		user.setVerified(true);
+		user.setSignUpDate(LocalDate.now());
+		user.setEmail("damian.tzn@outlook.com");
+		user.setPassword("admin");
+		user.setGender(Gender.MALE);
+		context.getBean(UserService.class).addUser(user);
 	}
 
 }
