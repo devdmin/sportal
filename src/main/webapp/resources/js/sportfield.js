5109cf52bbdbda4ee9c362c0c871621d5c75638a
devdmin <devdmin@protonmail.ch>
2017-04-12 20:45:36 +0200

    adding sportfields fixed

--- src/main/webapp/resources/js/sportfield.js
+++ src/main/webapp/resources/js/sportfield.js
@@ -7,7 +7,7 @@ function initAutocomplete() {
   });
     var newSportField;
     map.setOptions({draggableCursor:'crosshair'});
-  // Create the search box and link it to the UI element.
+//  // Create the search box and link it to the UI element.
   var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
