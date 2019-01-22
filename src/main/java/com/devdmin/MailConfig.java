c2b41bd723f7f5420bb64b28e8db878ee6af27ec
Damian Ujma <devdmin@protonmail.ch>
2019-01-22 19:42:00 +0100

    update

--- src/main/java/com/devdmin/MailConfig.java
+++ src/main/java/com/devdmin/MailConfig.java
@@ -15,8 +15,8 @@ public class MailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("");
-        mailSender.setPassword("");
+        mailSender.setUsername("sportalactivation@gmail.com");
+        mailSender.setPassword("cbf1c96cbf1c96");
         mailSender.setJavaMailProperties(getMailProperties());
 
         return mailSender;
